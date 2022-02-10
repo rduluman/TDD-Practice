120254e6a7cb3bbee9b0661b43ddadcf4c8a9f29
razvami <Razvan.Mihai@cegeka.com>
2022-02-10 11:30:24 +0200

    failing test

--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -1,12 +1,27 @@
+import org.junit.Before;
 import org.junit.Test;
+import vendingmachine.VendingMachine;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class VendingMachineTest {
 
+    private VendingMachine vendingMachine;
+
+    @Before
+    public void init() {
+        vendingMachine = new VendingMachine();
+    }
+
     @Test()
     public void classExists() throws ClassNotFoundException {
         assertThat(Class.forName("vendingmachine.VendingMachine").getName()).isEqualTo("vendingmachine.VendingMachine");
+    }
+
+    @Test()
+    public void givenColaIsSelected_then_colaIsReturned() {
+        vendingMachine.selectDrink(cola);
+        assertThat(vendingMachine.retrieveProduct()).isEqualTo(cola);
     }
 }
